004a6893dc7d68839293b8656831fda7a3b0e42e
Brahim SAHBI <brahim.sahbi@gmail.com>
2020-07-25 10:04:09 +0200

    Add alignement in HTML form

--- webapp/app.py
+++ webapp/app.py
@@ -1,14 +1,15 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, send_file
 from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 import sqlite3
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441677567d441f2b6176a'
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config.from_object(__name__)
 
 class ReusableForm(Form):
     title = TextField('Title:', validators=[validators.required()])
-    lyrics = TextAreaField('Lyrics:', validators=[validators.required()])
+    lyrics = TextAreaField('Lyrics:', validators=[validators.required()], render_kw={'cols': 80, 'rows': 40})
 
     @app.route("/new", methods=['GET', 'POST'])
     def insert():
@@ -23,10 +24,11 @@ class ReusableForm(Form):
                 print(title)
 
         if form.validate():
-            flash('Hello ' + title)
+            flash(title + ' inserted in database')
         else:
             flash('All the form fields are required. ')
-        return render_template('hello.html', form=form)
+        return render_template('insert.html', form=form)
+
 @app.route('/songs/<int:maxRowID>', methods=['GET'])
 def query(maxRowID):
     with sqlite3.connect("songs.sqlite") as conn:
@@ -45,6 +47,19 @@ def query(maxRowID):
             result += "}"
         result += ']}'
         return result
+
+@app.route('/songs.sqlite.dump', methods=['GET'])
+def database_dump():
+    with sqlite3.connect("songs.sqlite") as conn:
+        cursor = conn.cursor()
+        with open("songs.sqlite.dump", "w") as dump_file:
+            for line in conn.iterdump():
+                dump_file.write('%s\n' % line)
+    return send_file('songs.sqlite.dump')
+
+@app.route('/songs.sqlite', methods=['GET'])
+def database_file():
+    return send_file('songs.sqlite')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
