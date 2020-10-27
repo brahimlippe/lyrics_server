cd14e2f475d5ac200ae2c697aa3df7252c748fee
Brahim SAHBI <brahim.sahbi@gmail.com>
2020-10-27 18:44:56 +0100

    Fix some pylint errors

--- server.py
+++ server.py
@@ -1,56 +1,67 @@
-from flask import Flask, render_template, flash, request, send_file, redirect, url_for
-from smtplib import SMTP
-from email.message import EmailMessage
-from sqlalchemy.ext.serializer import dumps
+"""
+    One file server this allows users to add/modify and list all songs in the database
+    A contact page is also provided. A REST API is available to query songs.
+"""
 import os
 import json
+from smtplib import SMTP
+from email.message import EmailMessage
+from flask import Flask, render_template, flash, request
+from flask import send_file, redirect, url_for
+from flask.logging import create_logger
 from sqlalchemy import Table, Column, Integer, String, MetaData, create_engine
 
-app = Flask(__name__)
-app.secret_key = os.urandom(16)
-app.config.from_object(__name__)
+APP = Flask(__name__)
+APP.secret_key = os.urandom(16)
+APP.config.from_object(__name__)
+LOG = create_logger(APP)
 
-engine = create_engine(os.environ['DATABASE_URL'], echo = True)
-meta = MetaData()
-songs = Table(
-   'song', meta,
-   Column('songID', Integer, primary_key = True),
-   Column('title', String),
-   Column('lyrics', String),
-)
-meta.create_all(engine)
+ENGINE = create_engine(os.environ['DATABASE_URL'], echo=True)
+META = MetaData()
+SONG_TABLE = Table('song', META,
+                   Column('songID', Integer, primary_key=True),
+                   Column('title', String),
+                   Column('lyrics', String))
+META.create_all(ENGINE)
 
-@app.route("/new", methods=['GET', 'POST'])
+@APP.route("/new", methods=['GET', 'POST'])
 def insert():
+    """ Insert new song in the database """
     if request.method == 'POST':
-        query = songs.insert().values(title=request.form.get('title'),
-                                      lyrics=request.form.get('lyrics'))
-        conn = engine.connect()
+        query = SONG_TABLE.insert().values(title=request.form.get('title'),
+                                           lyrics=request.form.get('lyrics'))
+        conn = ENGINE.connect()
         conn.execute(query)
         flash('زيدت الأغنية "' + request.form.get('title') + '"', 'success')
     return render_template('insert.html.j2')
 
-@app.route("/modify/<int:id>", methods=['GET', 'POST'])
-def modify(id):
-    conn = engine.connect()
-    song_query = [song for song in conn.execute(songs.select().where(id == songs.c.songID))]
-    if len(song_query) == 0: return redirect(url_for('list'))
-    song = song_query[0]
+@APP.route("/modify/<int:song_id>", methods=['GET', 'POST'])
+def modify(song_id):
+    """ Display a song and modify via a form """
+    conn = ENGINE.connect()
+    songs = list(conn.execute(SONG_TABLE.select().where(song_id == SONG_TABLE.c.songID)))
+    if len(songs) == 0:
+        return redirect(url_for('song_list'))
+    song = songs[0]
     lyrics = song.lyrics
     title = song.title
     if request.method == 'POST':
         lyrics = request.form.get('lyrics')
         title = request.form.get('title')
-        stmt = songs.update().where(songs.c.songID == id).values(title=title, lyrics=lyrics)
+        where = SONG_TABLE.update().where(SONG_TABLE.c.songID == song_id)
+        stmt = where.values(title=title, lyrics=lyrics)
         conn.execute(stmt)
         flash('تم تغيير الأغنية "' + request.form.get('title') + '"', 'success')
-    return render_template('insert.html.j2', lyrics = lyrics, title = title)
+    return render_template('insert.html.j2', lyrics=lyrics, title=title)
 
-@app.route('/')
-def index(): return render_template('index.html.j2')
+@APP.route('/')
+def index():
+    """ Static index page """
+    return render_template('index.html.j2')
 
-@app.route('/contact', methods=['GET', 'POST'])
+@APP.route('/contact', methods=['GET', 'POST'])
 def contact():
+    """ Display ways to contact contributors """
     if request.method == 'POST':
         try:
             with SMTP('smtp.gmail.com:587', timeout=5) as server:
@@ -66,44 +77,47 @@ def contact():
                 email['Subject'] = "Poetic feelings contact"
                 server.send_message(email)
             flash("Message envoyé", "success")
-        except Exception as e:
-            app.logger.error(str(e))
-            flash("Une erreur technique est survenue, veuillez nous contacter par mail: brahim.pro@protonmail.com", "danger")
+        except Exception as exception:
+            LOG.error(str(exception))
+            flash("Une erreur technique est survenue,"\
+                  "veuillez nous contacter par mail:"\
+                  "brahim.pro@protonmail.com", "danger")
     return render_template("contact.html.j2")
 
-@app.route('/list', methods=['GET'])
-def list():
-    return render_template("list.html.j2", songs = engine.connect().execute(songs.select()))
+@APP.route('/list', methods=['GET'])
+def list_songs():
+    """ list all songs """
+    return render_template("list.html.j2", songs=ENGINE.connect().execute(SONG_TABLE.select()))
 
-@app.route('/songs/<int:maxRowID>', methods=['GET'])
-def query(maxRowID):
-    query = engine.connect().execute(songs.select().where(songs.c.songID > maxRowID))
+@APP.route('/songs/<int:max_row_id>', methods=['GET'])
+def song_query(max_row_id):
+    """ return a JSON with all songs whose id are greater than max_row_id """
+    query = ENGINE.connect().execute(SONG_TABLE.select().where(SONG_TABLE.c.songID > max_row_id))
     song_list = [{'songID': song.songID, 'title': song.title, 'lyrics': song.lyrics}
                  for song in query]
     return json.dumps({'songs': song_list}, ensure_ascii=False)
 
-@app.route('/songs.dump.sqlite', methods=['GET'])
+@APP.route('/songs.dump.sqlite', methods=['GET'])
 def database_dump():
+    """ Dump an sqlite replicate of the database """
     file_name = './static/songs.dump.sqlite'
     try:
         os.remove(file_name)
-    except:
+    except Exception:
         pass
     sqlite_engine = create_engine('sqlite:///' + file_name)
     sqlite_meta = MetaData()
-    sqlite_songs = Table(
-            'song', sqlite_meta,
-            Column('songID', Integer, primary_key = True),
-            Column('title', String),
-            Column('lyrics', String))
+    sqlite_songs = Table('song', sqlite_meta,
+                         Column('songID', Integer, primary_key=True),
+                         Column('title', String),
+                         Column('lyrics', String))
     sqlite_meta.create_all(sqlite_engine)
-    for song in engine.connect().execute(songs.select()):
-        app.logger.info(song.title)
+    for song in ENGINE.connect().execute(SONG_TABLE.select()):
         query = sqlite_songs.insert().values(title=song.title,
-                lyrics=song.lyrics,
-                songID=song.songID)
+                                             lyrics=song.lyrics,
+                                             songID=song.songID)
         sqlite_engine.connect().execute(query)
     return send_file(file_name)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    APP.run(host='0.0.0.0')
